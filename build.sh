0ab7ed4a1c2cebf52a460b8a0dfaee9778325cf7
rtrevorrow <rtrevorrow@codametrix.com>
2021-02-13 18:53:07 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ sudo usermod -a -G docker vagrant
 newgrp docker
 npm install --force
 npm run build
-docker build --rm -f Dockerfile.prod -t dashboard .
+docker build --rm -f Dockerfile.prod -t dashboard:latest .
 docker tag dashboard rtrevorrow/dashboard:latest
 docker push rtrevorrow/dashboard:latest
