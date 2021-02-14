c718c133e70cf5f4ce4afda31a3d4115f28fa854
rtrevorrow <rtrevorrow@codametrix.com>
2021-02-14 13:14:28 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # newgrp docker
 rm -rf node_modules/ || true
 rm -f package-lock.json/ || true
+echo "clearing cache"
 npm cache clean --force
 # npm install react-scripts
 # npm install typescript
@@ -11,6 +12,7 @@ ls
 echo "PATH IS"
 pwd
 npm run dbuild
+npm run dsrm || true
 # docker build --rm -f Dockerfile.prod -t dashboard:latest .
 # docker tag dashboard rtrevorrow/dashboard:latest
 # docker push rtrevorrow/dashboard:latest
